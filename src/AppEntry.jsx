99f04a78662a243717120e3f6b0a7cfa7cf5efee
Toni Baskijera <toni.baskijera@student.uniri.hr>
2022-09-20 10:00:11 +0200

    Fixed android status bar

--- src/AppEntry.jsx
+++ src/AppEntry.jsx
@@ -2,11 +2,18 @@ import "react-native-gesture-handler";
 import { Provider as PaperProvider } from "react-native-paper";
 import { AuthenticationContextProvider } from "@/services/authentication/AuthenticationContext";
 import { NavigationRouter } from "@/navigation/NavigationRouter";
+import { StatusBar } from "react-native";
+import { colorAccent } from "@/style/colors";
 
 export const AppEntry = () => {
   return (
     <AuthenticationContextProvider>
       <PaperProvider>
+        <StatusBar
+          backgroundColor="transparent"
+          translucent={true}
+          barStyle="dark-content"
+        />
         <NavigationRouter />
       </PaperProvider>
     </AuthenticationContextProvider>
